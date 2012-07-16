2953324eec8793a77ebcea585c7e4d83faf0f149
syedfaisalkarim <syedfaisalkarim@hotmail.com>
2012-07-16 22:40:24 +0530

    Update master

--- complex.h
+++ complex.h
@@ -1,41 +1,107 @@
+/*a simple class to use complex numbers
+ * provides basic operations
+ * T => float,double,long long int,int
+ */
+
 #include<cmath>
+#include<iostream>
 
 using namespace std;
 
+template < class T >
 class complex
 {
-  float r,i;
+  
+private:
+  T real,imaginary;
+  
 public:
-  complex(float a,float b)
+  
+  /*constructors
+   */
+  complex<T>(T a,T b)
   {
-    r=a;
-    i=b;
+    real=a;
+    imaginary=b;
   }
-  friend complex operator+(complex a,complex b)
+  
+  complex<T>()
   {
-    return complex(a.r+b.r,a.i+b.i);
+    real=0;
+    imaginary=0;
   }
-  friend complex operator-(complex a,complex b)
+  
+  complex<T>(const complex<T> &temp)
   {
-    return complex(a.r-b.r,a.i-b.i);
+    real=temp.real;
+    imaginary=temp.imaginary;
   }
-  friend complex operator*(complex a,complex b)
+  
+  /*operators and functions:-
+   * floor
+   * absolute value
+   * add
+   * subtract
+   * multiply
+   * divide
+   * modulus
+   */
+  
+  void floor()
   {
-    return complex(a.r*b.r-a.i*b.i,a.r*b.i+b.r*a.i);
+    real=std::floor(real);
+    imaginary=std::floor(imaginary);
   }
-  friend complex operator/(complex a,complex b)
+  
+  //for explanation of functions below assume complex number of type:- <<  a+ib  >>
+  
+  
+  /*absolute value of a+ib= a^2 + b^2
+   */
+  T abs()
   {
-    return complex((a.r*b.r+a.i*b.i)/(b.r*b.r+b.i*b.i),-a.r*b.i+b.r*a.i/(b.r*b.r+b.i*b.i));
+    return real*real+imaginary*imaginary;
   }
-  friend complex operator%(complex a,complex b)
+  
+  /*c1 + c2= ( a1 + a2 ) + i( b1 + b2 )
+   */
+  complex operator+(complex<T> param)
   {
-    complex c=a/b;
-    c.r=floor(c.r);
-    c.i=floor(c.i);
-    return (a-(c)*b);
+    return complex<T>(real+param.real,imaginary+param.imaginary);
   }
-  void print()
+  
+  /*c1 - c2= ( a1 - a2 ) + i( b1 - b2 )
+   */
+  complex operator-(complex<T> param)
   {
-    printf("\n%.2f+i%.2f\n",r,i);
+    complex<T> result(real-param.real,imaginary-param.imaginary);
+    return result;
+  }
+  
+  /*c1 * c2= ( a1 * a2 - b1 * b2 ) + i( a1 * b2 + a2 * b1 )
+   */
+  complex operator*(complex<T> param)
+  {
+    return complex<T>(real*param.real-imaginary*param.imaginary,real*param.imaginary+param.real*imaginary);
+  }
+  
+  /*c1 / c2 = ( a1 * a2 + b1 * b2 ) / ( a2 *a2 + b2 * b2 ) - i( a1 * b2 + a2 * b1 ) / ( a2 *a2 + b2 * b2 )
+   */
+  complex operator/(complex<T> param)
+  {
+    return complex<T>((real*param.real+imaginary*param.imaginary)/(param.real*param.real+param.imaginary*param.imaginary),-real*param.imaginary+param.real*imaginary/(param.real*param.real+param.imaginary*param.imaginary));
+  }
+  
+  complex operator%(complex<T> param)
+  {
+    complex<T> c;
+    c=*(this)/param;
+    c.floor();
+    return complex<T>(*(this)-(c)*param);
+  }
+  
+  friend ostream& operator<<(ostream& out,complex<T> param)
+  {
+    out<<param.real<<" + i"<<param.imaginary<<"\n";
   }
 };
